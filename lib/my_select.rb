c854e7b7628b8c56f20a69ec0ead878c5c602f3a
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-15 01:53:52 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
   i = 0
   arr = []
   while i < collection.length
-    arr << yield(collection[i])
+    #arr << yield(collection[i])
+    arr << yield(collection.proc(i))
     i += 1
   end
   return arr
